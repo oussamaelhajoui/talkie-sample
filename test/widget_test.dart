be20655509d8918c6f346c5dda2a7e7d0ba4ff70
oussamaelhajoui <o.elhajoui@student.fontys.nl>
2021-01-08 16:13:06 +0100

    modified test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,49 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// import 'dart:js_util';
+
+// import 'dart:js_util';
+//
+import 'package:connectySample/src/models/initPackage.dart';
+import 'package:connectySample/src/utils/call_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:connectycube_sdk/connectycube_sdk.dart';
 
 import 'package:connectySample/main.dart';
 
+// import '../lib/src/models/initPackage.dart';
+// import '../lib/src/utils/call_manager.dart';
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Shows CircularProgress indicator', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byWidget(CircularProgressIndicator()), findsNothing);
+  });
+  test('Testing initPackage models', () async {
+    // Build our package model
+    final CubeUser _user = new CubeUser();
+    final ConferenceClient _client = ConferenceClient.instance;
+    final ConferenceSession _session = null;
+    final CallManager _manager = null;
+    final initPackage = InitPackage.auto(_user, _client, _session, _manager);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify that our counter starts at 0.
+    expect(initPackage.user.login, null);
+  });
+  test('Testing initPackage model with login name', () async {
+    // Build our package model
+    final CubeUser _user = new CubeUser(login: 'johan');
+    final ConferenceClient _client = ConferenceClient.instance;
+    final ConferenceSession _session = null;
+    final CallManager _manager = null;
+    final initPackage = InitPackage.auto(_user, _client, _session, _manager);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(initPackage.user.login, 'johan');
   });
 }
